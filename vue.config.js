2840f28000c3f4aa54a7b5fce250b1910800fe4e
赖元博 <b10955b@163.com>
2021-06-17 01:39:08 +0800

    初始化项目

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      // extensions:[]
+    alias: {  //起别名
+      'assets': "@/assets",
+      'components': "@/components",
+      'network': "@/network",
+      'views': "@/views",
+      'common': "@/common",
+    }
+    }
+  }
+}
